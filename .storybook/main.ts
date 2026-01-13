7baaa830a28a31af68dbab9a5ce9c19b153dd2f8
Kenny Phong <kienwoon.phong@n2nconnect.com>
2026-01-13 17:30:21 +0800

    upgraded vite to ^7.3.1 and @vitejs/plugin-react to ^5.1.2

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -3,10 +3,9 @@ import type { StorybookConfig } from "@storybook/react-vite";
 const config: StorybookConfig = {
     stories: ["../src/**/*.stories.@(mdx|js|jsx|mjs|ts|tsx)"],
     addons: [
-        "@storybook/preset-create-react-app",
         "@storybook/addon-links",
         "@storybook/addon-essentials",
-        "@storybook/addon-interactions"        
+        "@storybook/addon-interactions"
     ],
     framework: {
         name: "@storybook/react-vite",
